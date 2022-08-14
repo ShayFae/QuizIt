1e161de55043acb366efc4e144cf5caba9dc353f
ShayFae <ifrahnoor108@gmail.com>
2022-08-14 01:16:14 -0400

    Fix carousel buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,15 +6,17 @@ export default function Home() {
   const [carousel, setCarousel] = useState('one');
 
   return(
-      <div className='home'>
-        <h1>Welcome to Quiz It</h1>
+      <div>
+        <h1 className='home'>Welcome to Quiz It</h1>
+        <section className='test'>
           <button className='carousel' onClick={() => setCarousel('one')}></button>
           <button className='carousel' onClick={() => setCarousel('two')}></button>
           <button className='carousel' onClick={() => setCarousel('three')}></button>
           <button className='carousel' onClick={() => setCarousel('four')}></button>
           <button className='carousel' onClick={() => setCarousel('five')}></button>
-
-        <section>
+          </section>
+        
+        <section className='home'>
           {carousel === 'one' &&  <article><img src='https://www.verywellfamily.com/thmb/L0mE1NUZdJr2kRoyCmfLAXtqxic=/500x350/filters:no_upscale():max_bytes(150000):strip_icc()/iStock-607040647-5a318cfcb39d030037c8ef1a.jpg' alt='' /></article>}
           {carousel === 'two' &&  <article><img src='http://dano.com.ng/wp-content/uploads/2017/07/family-WATCHING-TV--1024x512.jpg' alt='' /></article>}
           {carousel === 'three' &&  <article><img src='https://viralsolutions.net/wp-content/uploads/2019/06/shutterstock_749036344.jpg' alt='' /></article>}
